4667e82cb45c7fc6e5106a715e0d74e78c29b599
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2024-01-05 19:13:23 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from "react";
+import { Camera, CameraType } from "expo-camera";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const camRef = useRef(null);
+  const requestPermissionCamAsync = async () => {
+    return await Camera.requestCameraPermissionsAsync();
+  };
+  const requestPermissionAssetAsync = async () => {
+    return await MediaLibrary.requestPermissionsAsync();
+  };
+
+  useEffect(() => {
+    (async () => {
+      await requestPermissionAssetAsync();
+      await requestPermissionCamAsync();
+    })();
+  }, []);
+
+  function toggleCameraType() {
+    setType((current) => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  async function camGetPhoto() {
+    if (camRef && camRef.current) {
+      const data = await camRef.current.takePictureAsync();
+      const asset = await MediaLibrary.createAssetAsync(data.uri);
+      MediaLibrary.createAlbumAsync("Photos", asset);
+      console.log(asset);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} ref={camRef}>
+        <View style={styles.mainView}>
+          <TouchableOpacity onPress={toggleCameraType} style={styles.flipAreaLeft}>
+            <Text style={styles.flipText}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={camGetPhoto} style={styles.flipAreaRigth}>
+            <Text style={styles.flipText}>Tirar Foto</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -13,8 +53,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  camera: {
+    flex: 1,
+  },
+  mainView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  flipAreaLeft: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+  },
+  flipAreaRigth: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+  flipText: {
+    fontSize: 20,
+    marginBottom: 15,
+    color: "#FFFFFF",
   },
 });
